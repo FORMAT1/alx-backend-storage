f87b9f0457cb7e1284dbf5243f7ed35f0d2ffb35
FORMAT1 <olayiwolafm@gmail.com>
2023-12-20 21:21:03 +0000

    exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,45 +1,10 @@
 #!/usr/bin/env python3
-""" Module for Redis db """
+'''A module for using the Redis NoSQL data storage.
+'''
+import uuid
 import redis
-from uuid import uuid4
-from typing import Union, Callable, Optional
-
-
-UnionOfTypes = Union[str, bytes, int, float]
-
-
-class Cache:
-    """ Class for methods that operate a caching system """
-
-    def __init__(self):
-        """ Instance of the Redis db """
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    def store(self, data: UnionOfTypes) -> str:
-        """
-        Method takes a data argument and returns a string
-        """
-        self._key = str(uuid4())
-        self._redis.set(self._key, data)
-        return self._key
-
-    def get(self, key: str,
-            fn: Optional[Callable] = None) -> UnionOfTypes:
-        """
-        Retrieves data stored in redis using a key
-        converts the result/value back to the desired format
-        """
-        value = self._redis.get(key)
-        return fn(value) if fn else value
-
-    def get_str(self, value: str) -> str:
-        """ get a string """
-        return self.get(self._key, str)
-
-    def get_int(self, value: str) -> int:
-        """ get an int """
-        return self.get(self._key, int)
+from functools import wraps
+from typing import Any, Callable, Union
 
 
 def count_calls(method: Callable) -> Callable:
@@ -96,3 +61,42 @@ def replay(fn: Callable) -> None:
             fxn_input.decode("utf-8"),
             fxn_output,
         ))
+
+
+class Cache:
+    '''Represents an object for storing data in a Redis data storage.
+    '''
+    def __init__(self) -> None:
+        '''Initializes a Cache instance.
+        '''
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''Stores a value in a Redis data storage and returns the key.
+        '''
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
+
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        '''Retrieves a value from a Redis data storage.
+        '''
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        '''Retrieves a string value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        '''Retrieves an integer value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: int(x))
